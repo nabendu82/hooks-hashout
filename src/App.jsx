2e09a0d058d2b34670fad8e270a83210d9a4890f
nabendu82 <nabendu.biswas@gmail.com>
2025-04-11 10:49:27 +0530

    useReducer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
 import './App.css'
-import React from 'react'
-import DocTitleOne from './components/DocTitleOne'
-import DocTitleTwo from './components/DocTitleTwo'
+import React, { useReducer } from 'react'
+import GrandParent1 from './components/GrandParent1'
 
 // import GrandParent from './components/GrandParent';
 
 // export const UserContext = React.createContext();
 // export const ChannelContext = React.createContext();
 
+export const CountContext = React.createContext();
+
+const initialState = 0;
+const reducer = (state, action) => {
+    switch(action) {
+        case 'increment':
+            return state + 1;
+        case 'decrement':
+            return state - 1;
+        case 'reset':
+            return initialState;
+        default:
+            return state;
+    }
+}
+
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState);
+  
   return (
     // <UserContext.Provider value="Nabendu">
     //   <ChannelContext.Provider value="Hashout">
     //     <GrandParent />
     //   </ChannelContext.Provider>
     // </UserContext.Provider>
-    <>
-      <DocTitleOne />
-      <DocTitleTwo />
-    </>
+    <CountContext.Provider value={{ count, dispatch }}>
+      <GrandParent1 />
+    </CountContext.Provider>
   )
 }
 
---
+++ src/components/GrandParent1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Parent1 from './Parent1'
+
+const GrandParent1 = () => {
+    return (
+        <div>
+            <Parent1 />
+        </div>
+    )
+}
+
+export default GrandParent1
